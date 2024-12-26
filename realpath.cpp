d1e97dcb76fbda621210010fa522bb5326fac736
Matt Wilson <matthew@synesis.com.au>
2024-12-26 20:43:23 +1100

    initial commit

---
+++ realpath.cpp
@@ -0,0 +1,64 @@
+
+#include <platformstl/filesystem/path_functions.h>
+#include <winstl/error/error_desc.hpp>
+#include <winstl/filesystem/absolute_path.hpp>
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	stlsoft::string_slice_m_t const program_name = platformstl::get_executable_name_from_path(argv[0]);
+
+	switch (argc)
+	{
+	case 2:
+
+		{
+			winstl::absolute_path abs_path(argv[1]);
+
+			if (0 == abs_path.length())
+			{
+				winstl::error_desc ed;
+
+				std::cerr
+					<< program_name
+					<< ": failed to obtain absolute path for '"
+					<< argv[1]
+					<< "': "
+					<< ed
+					<< std::endl;
+
+				return EXIT_FAILURE;
+			}
+			else
+			{
+
+				std::cout
+					<< abs_path
+					<< std::endl;
+
+				return EXIT_SUCCESS;
+			}
+		}
+		break;
+	case 1:
+
+		std::cerr
+			<< "USAGE: "
+			<< program_name
+			<< " <relative-path>"
+			<< std::endl;
+
+		return EXIT_FAILURE;
+	default:
+
+		std::cerr
+			<< program_name
+			<< ": too many arguments; use --help for usage"
+			<< std::endl;
+
+		return EXIT_FAILURE;
+	}
+}
+
