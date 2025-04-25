49b5997d48099893a5a4a22e55e4fd8e1d1ada43
Matt Wilson <matthew@synesis.com.au>
2025-04-25 18:36:10 +1000

    ~ whitespace

--- realpath.cpp
+++ realpath.cpp
@@ -19,57 +19,57 @@
 
 int main(int argc, char* argv[])
 {
-	stlsoft::string_slice_m_t const program_name = platformstl::get_executable_name_from_path(argv[0]);
+    stlsoft::string_slice_m_t const program_name = platformstl::get_executable_name_from_path(argv[0]);
 
-	switch (argc)
-	{
-	case 2:
+    switch (argc)
+    {
+    case 2:
 
-		{
-			winstl::absolute_path abs_path(argv[1]);
+        {
+            winstl::absolute_path abs_path(argv[1]);
 
-			if (0 == abs_path.length())
-			{
-				winstl::error_desc ed;
+            if (0 == abs_path.length())
+            {
+                winstl::error_desc ed;
 
-				std::cerr
-					<< program_name
-					<< ": failed to obtain absolute path for '"
-					<< argv[1]
-					<< "': "
-					<< ed
-					<< std::endl;
+                std::cerr
+                    << program_name
+                    << ": failed to obtain absolute path for '"
+                    << argv[1]
+                    << "': "
+                    << ed
+                    << std::endl;
 
-				return EXIT_FAILURE;
-			}
-			else
-			{
+                return EXIT_FAILURE;
+            }
+            else
+            {
 
-				std::cout
-					<< abs_path
-					<< std::endl;
+                std::cout
+                    << abs_path
+                    << std::endl;
 
-				return EXIT_SUCCESS;
-			}
-		}
-		break;
-	case 1:
+                return EXIT_SUCCESS;
+            }
+        }
+        break;
+    case 1:
 
-		std::cerr
-			<< "USAGE: "
-			<< program_name
-			<< " <relative-path>"
-			<< std::endl;
+        std::cerr
+            << "USAGE: "
+            << program_name
+            << " <relative-path>"
+            << std::endl;
 
-		return EXIT_FAILURE;
-	default:
+        return EXIT_FAILURE;
+    default:
 
-		std::cerr
-			<< program_name
-			<< ": too many arguments; use --help for usage"
-			<< std::endl;
+        std::cerr
+            << program_name
+            << ": too many arguments; use --help for usage"
+            << std::endl;
 
-		return EXIT_FAILURE;
-	}
+        return EXIT_FAILURE;
+    }
 }
 
